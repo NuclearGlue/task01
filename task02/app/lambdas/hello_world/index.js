2de906dd954e8bf24f6035ef1c62bdcf447e8789
Oleksandr Sorokin <sanek1678@gmail.com>
2025-02-18 15:53:06 +0200

    Message fix

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -12,6 +12,7 @@ exports.handler = async (event) => {
       statusCode: 400,
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
+        statusCode: 400,
           message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${event.requestContext.http.method}`
       }),
   };
